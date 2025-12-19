2a64b49198c5df18b55266d66fd140ead803d806
anmolarpit <anmolarpit07@gmail.com>
2025-12-19 09:47:10 +0530

    initial

--- github/rps.py
+++ github/rps.py
@@ -1,36 +1,66 @@
 import random
-
-print("WELCOME TO ROCK|PAPER|SCISSORS")
-a= int(input("Choose \n1.Rock \n2.Paper \n3.Scissors \nEnter:"))
-if (a == 1):
+import keyboard
+import time
+res = 0 
+auto = 0
+while True:
+    
+ print("WELCOME TO ROCK|PAPER|SCISSORS")
+ a= int(input("Choose \n1.Rock \n2.Paper \n3.Scissors \nEnter:"))
+ if (a == 1):
     print("Your choice is Rock")
-elif (a == 2):
+ elif (a == 2):
     print("Your choice is Paper")
-elif (a == 3):
+ elif (a == 3):
     print("Your choice is Scissors")
 
-print("Now the computer choice is..")
-b= random.randint(1,3)
+ 
+ b= random.randint(1,3)
+ time.sleep(1.5)
+ if (b == 1):
+    auto="Rock"
+ elif (b == 2):
+    auto="Paper"
+ elif (b == 3):
+    auto="Scissors"
+ print("Now the computer choice is..", auto)
+ time.sleep(1)
 
-if (b == 1):
-    print("Rock")
-elif (b == 2):
-    print("Paper")
-elif (b == 3):
-    print("Scissors")
+ if (a == 1 and b== 1):
+    res="DRAW"
+ elif(a==1 and b == 2):res="LOST"
+ elif(a==1 and b==3):res="------WON------"
 
-if (a == 1 and b== 1):
-    print("DRAW")
-elif(a==1 and b == 2):print("LOST")
-elif(a==1 and b==3):print("------WON------")
+ if (a == 2 and b== 2):
+    res="DRAW"
+ elif(a ==2 and b == 3):res="LOST"
+ elif (a==2 and b==1):res="------WON------"
 
-if (a == 2 and b== 2):
-    print("DRAW")
-elif(a ==2 and b == 3):print("LOST")
-elif (a==2 and b==1):print("------WON------")
+ if (a == 3 and b== 3):
+    res="DRAW"
+ elif(a ==3 and b == 1):res="LOST"
+ elif(a == 3 and b ==2):res="------WON------"
 
-if (a == 3 and b== 3):
-    print("DRAW")
-elif(a ==3 and b == 1):print("LOST")
-elif(a == 3 and b ==2):print("------WON------")
+ if (res=="DRAW"):
+    print("SAME CHOICE, QUITE STRANGE...")
+ elif(res=="LOST"):
+    print("<<COMPUTER WINS>>")
+    print("YOU WILL WIN AGAIN NEXT TIME....")
+ elif(res=="------WON------"):
+    print("<<YOU WON>>")
+    print("SO PROUD SO PROUD")
+ time.sleep(2)
+ print("DO YOU WANT TO PLAY AGAIN? Y/N")
+ c= input()
+ time.sleep(1)
 
+   
+ if (c=="N" or c=="n"):
+    print("THANK YOU FOR PLAYING")
+    break
+ elif(c=="Y" or c=="y"):
+    pass
+ else:
+    print("enter a valid key")
+    pass
+ 
